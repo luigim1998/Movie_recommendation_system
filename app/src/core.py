c4b805b083bc9009c7a42969e0eb1df0973cf5ed
Luigi Muller <sluigimuller@gmail.com>
2021-05-12 18:41:06 -0400

    Queries

--- app/src/core.py
+++ app/src/core.py
@@ -19,13 +19,28 @@ class CreateNode:
 
     def find_popular_genre(self, genre_ids):
         with self.driver.session() as session:
-            find_popular_by_genre = session.write_transaction(self._find_popular_by_genre, genre_ids)
+            find_popular_by_genre = session.read_transaction(self._find_popular_by_genre, genre_ids)
             return find_popular_by_genre
     
     def create_user(self, name, username, password):
         with self.driver.session() as session:
             create_user = session.write_transaction(self._create_user, name, username, password)
             return create_user
+
+    def find_by_user(self, username):
+        with self.driver.session() as session:
+            find_movie_by_user = session.read_transaction(self._find_movie_by_user, username)
+            return find_movie_by_user
+    
+    def find_by_like(self, username):
+        with self.driver.session() as session:
+            find_movie_by_like = session.read_transaction(self._find_movie_by_like, username)
+            return find_movie_by_like
+    
+    def like_movie(self, username, movie_id):
+        with self.driver.session() as session:
+            movie_liked = session.write_transaction(self._like_movie, username, movie_id)
+            return movie_liked
 
     @staticmethod
     def _create_and_return_greeting(tx, id, genre_ids, title, overview, release_date, vote_average, backdrop_path):
@@ -46,7 +61,19 @@ class CreateNode:
     
     @staticmethod
     def _find_movie_by_user(tx, username):
-        result = tx.run("MATCH (f:Filme)-[:GOSTA]->(n:Pessoa) WHERE n.username = {} RETURN f;".format(username))
+        result = tx.run('MATCH (f:Filme)<-[:CURTIU]-(n:Pessoa) WHERE n.username = "{}" RETURN f;'.format(username))
+        return result.data()
+
+    @staticmethod
+    def _find_movie_by_like(tx, username):
+        query = 'MATCH (p:Pessoa)-[:CURTIU]->(f:Filme)<-[:CURTIU]-(p2:Person)-[:CURTIU]->(f2:Filme) WHERE p.name = "{}" WITH f2 WHERE NOT (p)-[:CURTIU]->(f2) RETURN f2, COUNT(f2) as f2_t ORDER BY f2_t DESC LIMIT 4'.format(username)
+        result = tx.run(query)
+        return result.data()
+    
+    @staticmethod
+    def _like_movie(tx, username, movie_id):
+        query = 'MATCH (p:Pessoa), (f:Filme) WHERE p.name = "{}" AND f.id = {} CREATE (a)-[:CURTIU]->(b)'.format(username, movie_id)
+        result = tx.run(query)
         return result.data()
 
 #altenative bolt://host.docker.internal:7687/ enable extra_host in docker-compose.yml
