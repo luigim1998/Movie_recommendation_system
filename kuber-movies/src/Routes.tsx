c491af664cb5ee85b46574ffe1e31b9e06643e70
Miller Monteiro <millerraycell@gmail.com>
2021-05-20 14:00:37 -0400

    busca

--- kuber-movies/src/Routes.tsx
+++ kuber-movies/src/Routes.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Switch , Route} from "react-router-dom";
+
 import NavBar from "./core/components/NavBar";
 import Gender from "./pages/Gender";
 import List from "./pages/List";
 import MovieDetails from "./pages/List/components/MovieDetails";
 import Login from "./pages/Login";
 import Create from "./pages/Create";
+import SearchMovie from './pages/Search';
 
 const Routes = () => {
     return ( 
@@ -14,15 +16,23 @@ const Routes = () => {
                 <Route exact path="/">
                     <Login />
                 </Route>
+
                 <Route path="/create" exact>
                     <Create />
                 </Route>
+
                 <Route path="/genres" exact>
                     <Gender />
                 </Route>
+
                 <Route path="/list" exact>
                     <List />
                 </Route>
+
+                <Route path="/search" exact>
+                    <SearchMovie />
+                </Route>
+
                 <Route path="/movie">
                         <MovieDetails />
                 </Route>
